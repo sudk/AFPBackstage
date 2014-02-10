da0cc55073f9fcbae5ec757478d3fec7a3367e99
DunKuai Su <sudunkuai@gmail.com>
2014-02-10 21:22:05 +0800

    worldcup news
    worldcup news xml

--- protected/config/params.php
+++ protected/config/params.php
@@ -5,4 +5,10 @@ return array(
         'asiapacificnews'=>"english/asia/asiapacificnews",
         'ukfootball'=>'SPORTDIRECT-PHOTO/rss/en/UKFOOTBALL',
     ),
+    'wc_service_rul'=>"http://soc.webservice.sportsflash.com.au/securewebservice.asmx/",
+    'user_name'=>'presidum',
+    'password'=>'p35r34si678u5',
+    'client_id'=>'168',
+    'u_p_b64'=>"cHJlc2lkdW06cDM1cjM0c2k2Nzh1NQ==",
+    'xml_cache_time'=>600,
 );
--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -74,4 +74,84 @@ class SiteController extends BaseController {
         $this->redirect(Yii::app()->homeUrl);
     }
 
+    public function actionWorldcup(){
+
+        $service=$_GET['service'];
+        $error_code=0;
+        $url=Yii::app()->params['wc_service_rul'];
+        $client_id=Yii::app()->params['client_id'];
+        //$user_name=Yii::app()->params['user_name'];
+        //$password=Yii::app()->params['password'];
+        //$b64_str=base64_encode($user_name.":".$password);
+        $b64_str=Yii::app()->params['u_p_b64'];
+        $data="clientId=".$client_id;
+        unset($_GET['clientId']);
+        unset($_GET['r']);
+        unset($_GET['service']);
+        if(count($_GET)){
+            foreach($_GET as $k=>$v){
+                $data.="&".$k."=".$v;
+            }
+        }
+        header("Content-Type:text/xml");
+        $url.=$service;
+
+        //检查缓存是否存在
+        $cache_key=md5($url.$data);
+        $tuData=Yii::app()->cache->get($cache_key);
+        if($tuData){echo $tuData;exit;}
+
+        $tuCurl = curl_init();
+        curl_setopt($tuCurl, CURLOPT_URL,$url);
+        curl_setopt($tuCurl, CURLOPT_VERBOSE, 0);
+        curl_setopt($tuCurl, CURLOPT_HEADER,false);
+        curl_setopt($tuCurl, CURLOPT_RETURNTRANSFER,true);
+        curl_setopt($tuCurl, CURLOPT_POST,true);
+        curl_setopt($tuCurl, CURLOPT_POSTFIELDS,$data);
+        curl_setopt($tuCurl, CURLOPT_TIMEOUT,20);
+        curl_setopt($tuCurl, CURLOPT_HTTPHEADER, array("Authorization:Basic ".$b64_str));
+        try{
+            $tuData = curl_exec($tuCurl);
+            if(!curl_errno($tuCurl)){
+                $info = curl_getinfo($tuCurl);
+                if($info['http_code']!=200){
+                    $desc="Remote server HTTP error http_code:".$info['http_code'];
+                    $error_code=1002;
+                }
+            } else {
+                $desc="Remote server HTTP link error,desc:".curl_error($tuCurl);
+                $error_code=1004;
+            }
+        }catch (Exception $e){
+            $desc="Remote server HTTP link error";
+            $error_code=1005;
+        }
+        curl_close($tuCurl);
+
+
+        if($error_code>0){
+            $error_xml=<<<EOF
+<CadabilityServices>
+<Error>
+<ID>Err{$error_code}</ID>
+<Name>{$desc}</Name>
+<Message>
+{$tuData}
+</Message>
+</Error>
+</CadabilityServices>
+EOF;
+
+            echo $error_xml;
+        }else{
+            //缓存时间
+            $xml_cache_time=Yii::app()->params['xml_cache_time'];
+            //设置缓存
+            Yii::app()->cache->set($cache_key,$tuData,$xml_cache_time);
+
+            echo $tuData;
+        }
+
+    }
+
 }
