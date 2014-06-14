f7caeef24781eabda511b15ec46c71dccaf29c8c
DunKuai Su <sudunkuai@gmail.com>
2014-06-15 02:32:50 +0800

    qw
    qw

--- protected/views/video/v16_9.php
+++ protected/views/video/v16_9.php
@@ -2,7 +2,7 @@
 <object id="ftbplayer" class="vrplayerstub">
 <param name="mode" value="16_9" />
 <param name="width" value="800" />
-<param name="clientID" value=" 51482982" />
+<param name="clientID" value="51482982" />
 <param name="lang" value="ENG" />
 <param name="version" value="3.0" />
 <param name="competition" value="wc2014" />
